fef5d8f9a54da9d8fb8ba3a5296357e83e7a636e
dyoonar <71246827+dyoonar@users.noreply.github.com>
2020-10-07 14:10:43 +0700

    Add files via upload

---
+++ tugas4-1.php
@@ -0,0 +1,13 @@
+<?php
+$b = 8000;
+$j = 7500;
+$u = 300;
+$h = round(($b-$j)/$u);
+
+
+print ("Harga Beli = ".$b." <br>");
+print ("Harga Jual = ".$j."<br>");
+print ("Keuntungan = ".$u."<br>");
+print ("Berapa Jumlah kambing yang dibeli? Jawaban : ".$h." ekor");
+
+?>
---
+++ tugas4-2.php
@@ -0,0 +1,12 @@
+<?php
+$j = 523;
+$a = 8891;
+$h = $a/$j;
+
+
+print ("Jumlah Saluran = ".$j." <br>");
+print ("Jumlah terpakai = ".$a."<br>");
+print ("Rata-rata Pemakaian = ...?<br>");
+print ("Jawaban : ".$h." Liter/keluarga");
+
+?>
